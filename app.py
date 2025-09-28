537219910112c183e2bf3bdb3285e188e6e5a20b
meron14725 <meron14725@yahoo.co.jp>
2025-09-28 20:37:14 +0900

    add: index.html

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route("/test")
-def hello_world_test():
-    return "<p>Hello, World!test!</p>"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
