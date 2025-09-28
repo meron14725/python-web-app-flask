f75b81ad1d5a84e9a9f30da02fa475863dc3c6f4
meron14725 <meron14725@yahoo.co.jp>
2025-09-28 21:44:29 +0900

    add:　デザイン作成

--- app.py
+++ app.py
@@ -3,9 +3,25 @@ from flask import render_template
 
 app = Flask(__name__)
 
+memo_list=[
+    {'title':'memo1', 'body':'memo1の内容'},
+    {'title':'memo2', 'body':'memo2の内容'},
+    {'title':'memo3', 'body':'memo3の内容'},
+    # {'title':'memo4', 'body':'memo4の内容'},
+    # {'title':'memo5', 'body':'memo5の内容'},
+    # {'title':'memo2', 'body':'memo2の内容'},
+    # {'title':'memo2', 'body':'memo2の内容'},
+    # {'title':'memo2', 'body':'memo2の内容'},
+    # {'title':'memo2', 'body':'memo2の内容'},
+    # {'title':'memo2', 'body':'memo2の内容'},
+    # {'title':'memo2', 'body':'memo2の内容'},
+    # {'title':'memo2', 'body':'memo2の内容'},
+]
+
 @app.route("/")
-def hello():
-    return render_template('index.html')
+@app.route("/top")
+def top():
+    return render_template('index.html', memo_list=memo_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
